85fb6462e607407ecfb4e312ce3ee64d08de11b3
Konyicska-Kristof <113448136+Konyicska-Kristof@users.noreply.github.com>
2023-02-09 10:09:51 +0100

    Add files via upload

---
+++ ADFGVX/ADFGVX/ADFGVX/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ADFGVX
+{
+    internal class Program
+    {
+        static string Kulcs;
+        static string Uzenet;
+        static void Main(string[] args)
+        {
+            Feladat2();
+            Feladat3();
+            //Feladat5();
+        }
+
+        private static void Feladat3()
+        {
+            Console.WriteLine("3. Feladat: ");
+            ADFGVXrejtjel TEST = new ADFGVXrejtjel(@"..\..\..\kodtabla\kodtabla.txt", Uzenet, Kulcs);
+
+
+            Console.WriteLine($"Átalakított üzenet EX: {TEST.Uzenet}");            //.AtalakitottUzenet(Uzenet)
+            Console.WriteLine(TEST.AtalakitottUzenet(Uzenet));          //NEM MŰKÖDIK
+
+        }
+
+        private static void Feladat5()
+        {
+            Console.Write("5. Feladat:");
+        }
+
+        private static void Feladat2()
+        {
+            Console.Write("2. Feladat:\n\tKérem a kulcsot [HOLD]: ");
+            Kulcs = Console.ReadLine().ToUpper();
+            Console.Write("\tKérem az üzenetet [Szeretem a csokit]: ");
+            Uzenet = Console.ReadLine().ToLower();
+            if (Uzenet=="")
+            {
+                Uzenet = "szeretem a csokit";
+            }
+            if (Kulcs=="")
+            {
+                Kulcs = "HOLD";
+            }            
+        }
+    }
+}
